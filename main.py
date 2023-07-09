e925de7f48d7078d24d9769329daa16632bb5f43
Zeeshankaif <zeeshankaif09@gmaail.com>
2023-07-09 19:29:16 +0530

    hotel booking

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv")
+
+
+class Hotel:
+    def __init__(self, id):
+     def book(self):
+        pass
+
+
+class ReservationTicket:
+    def generate(self):
+        pass
+
+print(df)
+id = input("Enter the id of hotel: ")
+hotel= Hotel(id)
